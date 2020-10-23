dcdbb16bf16c11ee5c9c5968cc4f3da918b9bcff
Srm-Akla <akellamahateja@gmail.com>
2020-10-23 16:43:23 +0200

    Adding LED_Matrix

---
+++ Sensehat/Led_Matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#LED_MATRIX Python3
+
+from sense_hat import SenseHat
+from time import sleep
+import random as rand
+import sys
+
+sense = SenseHat()
+
+while(True):
+    hled=rand.randrange(0,8)
+    vled=rand.randrange(0,8)
+    r=rand.randrange(0,255)
+    g=rand.randrange(0,255)
+    b=rand.randrange(0,255)
+    sense.set_pixel(hled,vled,[r,g,b])
+    print("x:{} y:{} r:{} g:{} b:{}".format(hled,vled,r,g,b))
+    sleep(float(sys.argv[1]))
+
+
+sense.clear()
